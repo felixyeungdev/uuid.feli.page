14bec95e10ee614bfb89a9b5e941b30106457cc3
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2020-12-28 01:49:51 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [uuid, setUuid] = useState(() => uuidv4());
+
+    function generateUUID(e) {
+        const uuid = uuidv4();
+        setUuid(uuid);
+    }
+
+    return (
+        <div className="App">
+            <CssBaseline></CssBaseline>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">UUID Generator</Typography>
+                </Toolbar>
+            </AppBar>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: "calc(100vh - 64px)" }}
+            >
+                <Card
+                    style={{
+                        minWidth: "350px",
+                    }}
+                >
+                    <CardContent>
+                        <Typography variant="h3">UUID:</Typography>
+                        <Typography variant="body2">{uuid}</Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button
+                            color="primary"
+                            variant="contained"
+                            onClick={generateUUID}
+                            style={{
+                                marginTop: "16px",
+                            }}
+                        >
+                            Generate
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,15 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+const reportWebVitals = (onPerfEntry) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        import("web-vitals").then(
+            ({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+                getCLS((data) => onPerfEntry("CLS", data));
+                getFID((data) => onPerfEntry("FID", data));
+                getFCP((data) => onPerfEntry("FCP", data));
+                getLCP((data) => onPerfEntry("LCP", data));
+                getTTFB((data) => onPerfEntry("TTFB", data));
+            }
+        );
+    }
 };
 
 export default reportWebVitals;
